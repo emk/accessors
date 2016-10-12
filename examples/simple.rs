22f47c2de093662c8658d3cf55ebd0542639826c
Eric Kidd <git@randomhacks.net>
2016-10-12 17:20:47 -0400

    Shorten example code by removing `new`

--- examples/simple.rs
+++ examples/simple.rs
@@ -9,14 +9,8 @@ struct Simple {
     field: String,
 }
 
-impl Simple {
-    pub fn new(field: String) -> Simple {
-        Simple { field: field }
-    }
-}
-
 fn main() {
-    let mut s = Simple::new("hello".to_owned());
+    let mut s = Simple { field: "hello".to_owned() };
     println!("{}", s.field());
     s.set_field("there");
 }
